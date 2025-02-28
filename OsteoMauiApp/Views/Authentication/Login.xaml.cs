b8b50f90667c68997b858c212bb3fa04cdc0796a
salmanpervazA2 <134609629+salmanpervazA2@users.noreply.github.com>
2025-02-28 15:05:07 +0500

    commit

--- OsteoMauiApp/Views/Authentication/Login.xaml.cs
+++ OsteoMauiApp/Views/Authentication/Login.xaml.cs
@@ -19,6 +19,26 @@ public partial class Login : ContentPage
         BindingContext = _loginVM;
 
     }
+
+    private void OnFrameTappedPractitioner(object sender, EventArgs e)
+    {
+        PractitionerTextBox.IsVisible = true;
+        PractitionerFrame.Stroke = Color.FromArgb("#00d9bc");
+        PatientTextBox.IsVisible = false;
+        phoneinput.Text = "";
+        PatientFrame.Stroke = Color.FromArgb("#00FFFFFF");
+        _loginVM.SelectedUserType = 2;
+    }
+    private void OnFrameTappedPatient(object sender, EventArgs e)
+    {
+        PractitionerTextBox.IsVisible = false;
+        emailinput.Text = "";
+        PractitionerFrame.Stroke = Color.FromArgb("#00FFFFFF");
+        PatientTextBox.IsVisible = true;
+        PatientFrame.Stroke = Color.FromArgb("#00d9bc");
+        _loginVM.SelectedUserType = 1;
+    }
+
     private async void OnSignupClicked(object sender, EventArgs e)
     {
         _loginVM.SignupCommand.Execute(null);
